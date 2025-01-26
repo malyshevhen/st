607143f8526e889d3e494713e6b645c12facc11a
Evhen Malysh <malysh.evgeniy@gmail.com>
2025-01-26 14:52:46 +0200

    Back to No Ligatures SF Font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liga SFMono Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font = "SFMono Nerd Font Mono:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
